779aa3e34c2f8510437f42b274988d14744f888f
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2019-11-06 14:47:08 +0000

    + generic command classes module

---
+++ command/generic_command_classes.py
@@ -0,0 +1,7 @@
+from .abstract_class_definitions import command_primitive
+
+
+class single_value_data(command_primitive):
+    def __init__(self, name):
+        self.name = name
+        self.value = None
