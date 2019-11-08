55ab8be82d53231bd9778acb971ea2da88df0c70
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2019-11-08 18:16:18 +0000

    + value update test

--- tests_and_demos/_tests.py
+++ tests_and_demos/_tests.py
@@ -1,6 +1,23 @@
 from CommsAndCommand import command
 
 
+def print_troubling_set_attrs(generic_set):
+    print(
+        "set excluded_properties",
+        generic_set._excluded_properties,
+        "---",
+        "set structure",
+        generic_set.get_structure(),
+        "---",
+        "set json",
+        generic_set.get_json(),
+        "---",
+        "set repr",
+        generic_set,
+        sep="\n"
+    )
+
+
 def test_json():
     # proof test json
     generic_command = command.single_value_command(name="generic_command")
@@ -19,19 +36,25 @@ def test_cmd_set():
         generic_command_3,
         name="generic_set"
     )
-    print(
-        "set excluded_properties",
-        generic_set._excluded_properties,
-        "---",
-        "set structure",
-        generic_set.get_structure(),
-        "set json",
-        generic_set.get_json(),
-        "set repr",
-        generic_set,
-        sep="\n"
+    print_troubling_set_attrs(generic_set)
+
+
+def test_cmd_set_continued():
+    generic_command_1 = command.single_value_command(name="generic_command_1")
+    generic_command_2 = command.single_value_command(name="generic_command_2")
+    generic_command_3 = command.single_value_command(name="generic_command_3")
+    generic_set = command.command_set(
+        generic_command_1,
+        generic_command_2,
+        generic_command_3,
+        name="generic_set"
     )
+    print("update test: before")
+    print_troubling_set_attrs(generic_set)
+    generic_command_1.value = "dumb value"
+    print("update test: after setting command_1 value")
+    print_troubling_set_attrs(generic_set)
 
 
 # basic_command_functionality()
-test_cmd_set()
+test_cmd_set_continued()
