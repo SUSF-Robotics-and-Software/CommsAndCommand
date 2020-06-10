5f59636e9784fd4733230049d2b85af2564f7f02
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-06-10 18:26:23 +0100

    added client runner to main file

--- comms/src/lib.rs
+++ comms/src/lib.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::str::from_utf8;
+
 
 fn handle_client(mut stream: TcpStream) {
     let mut data = [0 as u8; 50];
@@ -20,7 +22,42 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 
-fn main() {
+
+fn run_client() {
+    match TcpStream::connect("localhost:12864") {
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port 12864");
+
+            let msg = b"Hello!";
+
+            stream.write(msg).unwrap();
+            println!("Sent Hello, awaiting reply...");
+
+            let mut data = [0 as u8; 6]; // using 6 byte buffer
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    if &data == msg {
+                        println!("Reply is ok!");
+                    } else {
+                        let text = from_utf8(&data).unwrap();
+                        println!("Unexpected reply: {}", text);
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Terminated.");
+}
+
+
+
+fn run_server() {
     let listener = TcpListener::bind("0.0.0.0:12864").unwrap();
     println!("Server listening on port 12864");
     for stream in listener.incoming() {
