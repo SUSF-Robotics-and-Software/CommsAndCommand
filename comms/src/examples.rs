3eb2bcde2eaf751e32fc8766911688834a56096b
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2020-06-11 00:43:17 +0100

    moved stackoverflow code to example.rs

---
+++ comms/src/examples.rs
@@ -0,0 +1,80 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50];
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occured, terminating with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {
+        // pass
+    }
+}
+
+
+
+pub fn run_client() {
+    match TcpStream::connect("localhost:12864") {
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port 12864");
+
+            let msg = b"Hello!";
+
+            stream.write(msg).unwrap();
+            println!("Sent Hello, awaiting reply...");
+
+            let mut data = [0 as u8; 6]; // using 6 byte buffer
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    if &data == msg {
+                        println!("Reply is ok!");
+                    } else {
+                        let text = from_utf8(&data).unwrap();
+                        println!("Unexpected reply: {}", text);
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect: {}", e);
+        }
+    }
+    println!("Terminated.");
+}
+
+
+
+pub fn run_server() {
+    let listener = TcpListener::bind("0.0.0.0:12864").unwrap();
+    println!("Server listening on port 12864");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
+}
